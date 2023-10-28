0bc60fa74d761e1ac84b109dc74c63c9e8c5207d
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-10-27 19:43:58 -0700

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,15 @@
-#This code will install the package puppet-lint
+# Puppet manifest to install Flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Ensure pip is installed
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+  require => Package['python3-pip'],
 }
 
