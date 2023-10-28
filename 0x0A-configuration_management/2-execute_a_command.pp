c94dcf070a640b1b94047b67655ad4fe10469587
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-10-27 20:11:51 -0700

    exec

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
-# kill process killmenow
-
-exec { 'pkill':
-  command => 'pkill killmenow',
+# This code will kill process and works together with killmenow file.
+exec { 'killmenow':
+  command  => '/usr/bin/pkill killmenow',
   provider => 'shell',
+  return   => [0, 1],
 }
 
